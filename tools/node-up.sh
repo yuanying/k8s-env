26f74acd7d70a3538ad2b2e486c699744caca710
OTSUKA, Yuanying <yuanying@fraction.jp>
2017-06-13 14:51:02 +0900

    Change NODE_HOST to NODE_HOSTNAME

--- tools/node-up.sh
+++ tools/node-up.sh
@@ -3,7 +3,7 @@
 set -eu
 export LC_ALL=C
 
-NODE_HOST=${NODE_HOST:-$1}
+NODE_HOSTNAME=${NODE_HOSTNAME:-$1}
 NODE_CPU=${NODE_CPU:-'2'}
 NODE_MEMORY=${NODE_MEMORY:-'2048'}
 NODE_DISK=${NODE_DISK:-'40'}
@@ -19,27 +19,27 @@ if [ ! -d ${LIBVIRT_PATH} ]; then
 fi
 
 
-echo "Setup USERDATA: ${NODE_HOST} node..."
+echo "Setup USERDATA: ${NODE_HOSTNAME} node..."
 
-USERDATA_DIR_PATH=${LIBVIRT_PATH}/${NODE_HOST}/openstack/latest
+USERDATA_DIR_PATH=${LIBVIRT_PATH}/${NODE_HOSTNAME}/openstack/latest
 if [ ! -d ${USERDATA_DIR_PATH} ]; then
     sudo mkdir -p ${USERDATA_DIR_PATH} || (echo "Can not create ${USERDATA_DIR_PATH} directory" && exit 1)
 fi
 sudo cp ${NODE_USERDATA} ${USERDATA_DIR_PATH}/user_data
-USERDATA_DISK="--filesystem ${LIBVIRT_PATH}/$NODE_HOST/,config-2,type=mount,mode=squash"
+USERDATA_DISK="--filesystem ${LIBVIRT_PATH}/$NODE_HOSTNAME/,config-2,type=mount,mode=squash"
 
 
-echo "Creating: ${NODE_HOST} node..."
+echo "Creating: ${NODE_HOSTNAME} node..."
 
-if [ ! -f ${LIBVIRT_PATH}/${NODE_HOST}.qcow2 ]; then
-    sudo cp ${NODE_IMAGE} ${LIBVIRT_PATH}/${NODE_HOST}.qcow2
-    sudo qemu-img resize ${LIBVIRT_PATH}/${NODE_HOST}.qcow2 ${NODE_DISK}G
+if [ ! -f ${LIBVIRT_PATH}/${NODE_HOSTNAME}.qcow2 ]; then
+    sudo cp ${NODE_IMAGE} ${LIBVIRT_PATH}/${NODE_HOSTNAME}.qcow2
+    sudo qemu-img resize ${LIBVIRT_PATH}/${NODE_HOSTNAME}.qcow2 ${NODE_DISK}G
 fi
 dev_index='b'
 additional_disk=''
 additional_disk_params=''
 for disk_size in ${NODE_ADDITIONAL_DISKS}; do
-    additional_disk=${LIBVIRT_PATH}/${NODE_HOST}-vd${dev_index}.img
+    additional_disk=${LIBVIRT_PATH}/${NODE_HOSTNAME}-vd${dev_index}.img
     if [ ! -f ${additional_disk} ]; then
         sudo qemu-img create ${additional_disk} ${disk_size}G
     fi
@@ -49,13 +49,13 @@ done
 
 sudo virt-install --connect qemu:///system \
              --import \
-             --name ${NODE_HOST} \
+             --name ${NODE_HOSTNAME} \
              --ram ${NODE_MEMORY} \
              --vcpus ${NODE_CPU} \
              --network bridge=${NODE_NETWORK_HOST_BRIDGE} \
              --os-type=linux \
              --os-variant=virtio26 \
-             --disk path=${LIBVIRT_PATH}/${NODE_HOST}.qcow2,format=qcow2,bus=virtio \
+             --disk path=${LIBVIRT_PATH}/${NODE_HOSTNAME}.qcow2,format=qcow2,bus=virtio \
              ${additional_disk_params} \
              ${USERDATA_DISK} \
              --vnc \

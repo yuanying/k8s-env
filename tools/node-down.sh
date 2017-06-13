8e16fd97164141abe188ca37b6ddc7bb5e9d742d
OTSUKA, Yuanying <yuanying@fraction.jp>
2017-06-13 18:27:57 +0900

    Update to use NODE_ENV to delete node

--- tools/node-down.sh
+++ tools/node-down.sh
@@ -2,7 +2,13 @@
 
 export LC_ALL=C
 
-NODE_HOSTNAME=${NODE_HOSTNAME:-$1}
+NODE_ENV=${1:-""}
+script_dir=$(dirname "${BASH_SOURCE}")
+
+if [[ ${NODE_ENV} != '' ]]; then
+    source ${NODE_ENV}
+fi
+
 LIBVIRT_PATH=${LIBVIRT_PATH:-'/var/lib/libvirt/images'}
 
 sudo virsh destroy ${NODE_HOSTNAME}

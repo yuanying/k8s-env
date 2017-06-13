50b8139d2f87674b97982d21558ee5d32aab6328
OTSUKA, Yuanying <yuanying@fraction.jp>
2017-06-13 15:21:24 +0900

    Add undefine to remove

--- tools/node-down.sh
+++ tools/node-down.sh
@@ -6,4 +6,5 @@ NODE_HOSTNAME=${NODE_HOSTNAME:-$1}
 LIBVIRT_PATH=${LIBVIRT_PATH:-'/var/lib/libvirt/images'}
 
 sudo virsh destroy ${NODE_HOSTNAME}
+sudo virsh undefine ${NODE_HOSTNAME}
 sudo rm -rf ${LIBVIRT_PATH}/${NODE_HOSTNAME}*

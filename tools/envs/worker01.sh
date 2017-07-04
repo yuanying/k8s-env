801a53ac9943d861e484deb56bcb287a09474fb8
OTSUKA, Yuanying <yuanying@fraction.jp>
2017-07-04 15:04:29 +0900

    Update to adjust current configuration

--- tools/envs/worker01.sh
+++ tools/envs/worker01.sh
@@ -6,6 +6,7 @@ source ${env_dir}/${NODE_OS_DISTRO}.sh
 
 export NODE_HOSTNAME="${NODE_OS_DISTRO}-worker01"
 export NODE_ADDRESS="${NODE_ADDRESS_PREFIX}.121"
+export NODE_MEMORY=4096
 
 export NODE_ADDITIONAL_DISKS='5'
 export NODE_USERDATA=${env_dir}/../userdata/${NODE_OS_DISTRO}-worker01.yaml

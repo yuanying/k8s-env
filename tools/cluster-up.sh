9189299d2ba87c913ae89b21d3ca81c9cd2f2b21
OTSUKA, Yuanying <yuanying@fraction.jp>
2017-08-24 12:38:00 +0900

    Add conbinient script to start cluster

---
+++ tools/cluster-up.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+bash tools/node-up.sh tools/envs/master01.sh
+bash tools/node-up.sh tools/envs/master02.sh
+bash tools/node-up.sh tools/envs/master03.sh
+
+bash tools/node-up.sh tools/envs/worker01.sh
+bash tools/node-up.sh tools/envs/worker02.sh
+bash tools/node-up.sh tools/envs/worker03.sh
+bash tools/node-up.sh tools/envs/worker04.sh
+bash tools/node-up.sh tools/envs/worker05.sh
+bash tools/node-up.sh tools/envs/worker06.sh

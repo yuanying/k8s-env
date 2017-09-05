0b95406cc59b75c49ef7a89d3cb210e28f3f4c10
OTSUKA, Yuanying <yuanying@fraction.jp>
2017-09-05 17:34:35 +0900

    Fix hardcoded image variable

--- tools/envs/ubuntu.sh
+++ tools/envs/ubuntu.sh
@@ -4,7 +4,7 @@ env_dir=$(dirname "${BASH_SOURCE}")
 source ${env_dir}/common.sh
 
 export NODE_OS_DISTRO='ubuntu'
-export NODE_IMAGE="/var/lib/libvirt/images/ubuntu-xenial-docker-ec2-noclouds.qcow2"
+export NODE_IMAGE=${NODE_IMAGE:-'/var/lib/libvirt/images/ubuntu-xenial-docker-ec2-noclouds.qcow2'}
 export NODE_ADDRESS_PREFIX=${NODE_ADDRESS_PREFIX:-'192.168.201'}
 export NODE_NET_DEVICE='ens3'
 

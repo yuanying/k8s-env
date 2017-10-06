28852be128c6f544da52216b5f00d9b92842d8ea
OTSUKA, Yuanying <yuanying@fraction.jp>
2017-10-06 13:27:04 +0900

    Update yuanying/keepalived image to v0.2.1

--- dockerfiles/keepalived/start.sh
+++ dockerfiles/keepalived/start.sh
@@ -1,6 +1,19 @@
 #!/bin/sh -e
 
 DAEMON=keepalived
+KEEPALIVED_CONFIG=${KEEPALIVED_CONFIG:-"/etc/keepalived/keepalived.cfg"}
+POD_IP=${POD_IP:-""}
+
+if [ "${POD_IP}" != "" ]; then
+    echo "POD_IP: ${POD_IP}"
+    NET_IFACE=$(ip -f inet -o addr | grep ${POD_IP} | cut -d' ' -f 2)
+    echo "NET_IFACE: ${NET_IFACE}"
+    echo "$(sed s/__NET_IFACE__/${NET_IFACE}/g ${KEEPALIVED_CONFIG})" > ${KEEPALIVED_CONFIG}
+else
+    echo "POD_IP is null"
+fi
+
+
 
 stop() {
     echo "Received SIGINT or SIGTERM. Shutting down $DAEMON"

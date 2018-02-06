c1afd7b51de8604f5e18ceebb409674b58f6389d
OTSUKA, Yuanying <yuanying@fraction.jp>
2018-02-06 16:34:55 +0900

    Wait glusterfs available

--- bin/gluster-configure-trusted-pool.sh
+++ bin/gluster-configure-trusted-pool.sh
@@ -7,6 +7,18 @@ ROOT=$(dirname "${BASH_SOURCE}")
 
 source ${ROOT}/../configs/bin/glusterfs.sh
 
+CLUSTER_STATUS='pending'
+echo "Waiting gluster cluster become available..."
+while [[ ${CLUSTER_STATUS} != 'Running' ]]; do
+  kubectl -n storage get pod
+  CLUSTER_STATUS=$(kubectl \
+  -n storage \
+  get pod -o go-template="{{range .items}}{{.status.phase}}{{\"\n\"}}{{end}}" \
+  | sort | uniq)
+  sleep 3
+done
+
+
 for NODE in ${GLUSTER_NODE}; do
   kubectl get pod -n storage --selector glusterfs-node=pod -o json | \
       jq ".items[0].metadata.name" -r | \

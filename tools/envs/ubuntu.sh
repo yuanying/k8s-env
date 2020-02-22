c71e2bd2e547287bb6e56122b7852445dcf455af
OTSUKA, Yuanying <mototsuk@zlab.co.jp>
2020-02-22 07:31:59 +0000

    Use bionic instead of xenial (1)

--- tools/envs/ubuntu.sh
+++ tools/envs/ubuntu.sh
@@ -4,8 +4,18 @@ env_dir=$(dirname "${BASH_SOURCE}")
 source ${env_dir}/common.sh
 
 export NODE_OS_DISTRO='ubuntu'
-export NODE_IMAGE=${NODE_IMAGE:-'/var/lib/libvirt/images/ubuntu-xenial-docker-ec2-noclouds.qcow2'}
+export NODE_OS_CODENAME='bionic'
+export NODE_OS_URL="http://cloud-images.ubuntu.com/${NODE_OS_CODENAME}/current/${NODE_OS_CODENAME}-server-cloudimg-amd64.img"
+export NODE_IMAGE_ORG=${NODE_IMAGE_ORG:-"/var/lib/libvirt/images/${NODE_OS_CODENAME}-server-cloudimg-amd64.img"}
+export NODE_IMAGE=${NODE_IMAGE:-"/var/lib/libvirt/images/${NODE_OS_CODENAME}-server-cloudimg-amd64.qcow2"}
 export NODE_ADDRESS_PREFIX=${NODE_ADDRESS_PREFIX:-'192.168.201'}
 export NODE_NET_DEVICE='ens3'
 
 export NODE_ADDITIONAL_DISKS=''
+
+if [ ! -f ${NODE_IMAGE_ORG} ]; then
+        sudo bash -c "wget ${NODE_OS_URL} -O - > ${NODE_IMAGE_ORG} || (rm -f ${NODE_IMAGE_ORG} && echo 'Failed to download image' && exit 1)"
+fi
+if [ ! -f ${NODE_IMAGE} ]; then
+        sudo qemu-img create -f qcow2 -b ${NODE_IMAGE_ORG} ${NODE_IMAGE}
+fi

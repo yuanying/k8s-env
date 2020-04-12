5b195989fd4f0ff196640a83deafd844e5a6dcff
OTSUKA, Yuanying <mototsuk@zlab.co.jp>
2020-04-12 05:41:25 +0000

    Increase memory and disk

--- tools/envs/worker04.sh
+++ tools/envs/worker04.sh
@@ -6,6 +6,7 @@ source ${env_dir}/${NODE_OS_DISTRO}.sh
 
 export NODE_HOSTNAME="${NODE_OS_DISTRO}-worker04"
 export NODE_ADDRESS="${NODE_ADDRESS_PREFIX}.124"
+export NODE_MEMORY=8192
 
 export NODE_ADDITIONAL_DISKS='80 80'
 export NODE_USERDATA=${env_dir}/../userdata/${NODE_OS_DISTRO}-worker04.yaml

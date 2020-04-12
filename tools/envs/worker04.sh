0c771a33090fc00f46659788500fe330a51c825d
OTSUKA, Yuanying <mototsuk@zlab.co.jp>
2020-04-12 05:31:49 +0000

    Expand node124 disk to 160GB

--- tools/envs/worker04.sh
+++ tools/envs/worker04.sh
@@ -7,5 +7,5 @@ source ${env_dir}/${NODE_OS_DISTRO}.sh
 export NODE_HOSTNAME="${NODE_OS_DISTRO}-worker04"
 export NODE_ADDRESS="${NODE_ADDRESS_PREFIX}.124"
 
-export NODE_ADDITIONAL_DISKS='10 10'
+export NODE_ADDITIONAL_DISKS='80 80'
 export NODE_USERDATA=${env_dir}/../userdata/${NODE_OS_DISTRO}-worker04.yaml

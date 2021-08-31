4e99b00a4971f47a9ec26c535c1b57c2a77df2b2
OTSUKA, Yuanying <mototsuk@zlab.co.jp>
2021-08-31 01:46:04 +0000

    Download authorized_keys from github

--- tools/envs/common.sh
+++ tools/envs/common.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+export NODE_GITHUB_USER=${NODE_GITHUB_USER:-'yuanying'}
 export NODE_ROOT=${NODE_ROOT:-'/var/vms'}
 export NODE_GATEWAY=${NODE_GATEWAY:-'192.168.11.1'}
 export NODE_DNS=${NODE_DNS:-'192.168.11.162'}
